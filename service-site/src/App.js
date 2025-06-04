854b6eca1ea2aad7a26253a143e304a1bace5165
YimighH34 <ming8582@hotmail.com>
2025-06-04 01:08:52 -0400

    added routers

--- service-site/src/App.js
+++ service-site/src/App.js
@@ -2,18 +2,49 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <div className="white-gradient"/>
-        <NavBar/>
-        <Hero/>
-      </div>
-      <Services/>
-      
-    </div>
+    //   <Router>
+    //
+    //
+    // <div className="App">
+    //   <div>
+    //     <div className="white-gradient"/>
+    //     <NavBar/>
+    //     <Hero/>
+    //   </div>
+    //   <Services/>
+    //
+    // </div>
+    //   </Router>
+
+
+
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <div className="white-gradient" />
+                <Hero />
+                <Services />
+              </>
+            } />
+
+            <Route path="/contact" element={
+              <Contact />
+            } />
+
+
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
---
+++ service-site/src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import MapView from '../components/MapView'; // if you have a map
+import '../App.css'; // or a separate Contact.css if needed
+
+function Contact() {
+    return (
+        <div className="contact-page">
+            <h1>Contact Us</h1>
+            <p>We're happy to hear from you! Here's how you can reach us:</p>
+            {/*<MapView /> /!* Custom map component *!/*/}
+        </div>
+    );
+}
+
+export default Contact;
