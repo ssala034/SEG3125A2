f8b7962c0807f2acc668881fcc1f01ff828f2564
ssala034 <sh.salat@gmail.com>
2025-06-08 16:17:33 -0400

    Changes

--- service-site/src/components/Footer.js
+++ service-site/src/components/Footer.js
@@ -1,33 +1,38 @@
 import React from 'react'
 import './Footer.css'
 import logo from '../assets/logo.png'
+
 const Footer = () => {
   return (
-    <section className="f-wrapper">
+    <>
+      <section className="f-wrapper">
         <div className="paddings innerWidth flexCenter f-container">
-            {/*left side */}
-            <div className="flexColStart f-left">
-                <a href="/">
-                    <img src={logo} alt='Footerlogo' width={100}/>
-                </a>
-                <span className="secondaryText">
-                    Our visions is to make all people <br/>
-                    be more curious and love learning.
-                </span>
+          {/* left side */}
+          <div className="flexColStart f-left">
+            <a href="/">
+              <img src={logo} alt='Footerlogo' width={100}/>
+            </a>
+            <span className="secondaryText">
+              Our visions is to make all people <br/>
+              be more curious and love learning.
+            </span>
+          </div>
+          {/* right side */}
+          <div className="flexColStart f-right">
+            <span className='primaryText'>Information</span>
+            <span className='secondaryText'> 75 Laurier Ave E, Ottawa, CA</span>
+            <div className="flexCenter f-menu">
+              <a href="/about" className="f-link">About Us</a>
+              <a href="/pricings" className="f-link">Packages</a>
+              <a href="/contact" className="f-link">Contact</a>
             </div>
-            {/*right side */}
-            <div className="flexColStart f-right">
-                <span className='primaryText'>Information</span>
-                <span className='secondaryText'> 75 Laurier Ave E, Ottawa, CA</span>
-                <div className="flexCenter f-menu">
-                    <a href="/about" className="f-link">About Us</a>
-                    <a href="/pricings" className="f-link">Packages</a>
-                    <a href="/contact" className="f-link">Contact</a>
-                </div>
-            </div>
-
+          </div>
         </div>
-    </section>
+      </section>
+      <div className="footer-credit secondaryText">
+        Designed by Shuaib and Yiming | © 2025 All rights reserved
+      </div>
+    </>
   )
 }
 
